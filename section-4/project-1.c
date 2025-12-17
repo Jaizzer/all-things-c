b0d447e42b11bd8b15b015e3e47f465c8c9855d4
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-12-17 08:42:57 +0800

    feat: Modify the code to reverse 3 digit numbers

--- section-4/project-1.c
+++ section-4/project-1.c
@@ -2,15 +2,19 @@
 
 int main(void) {
     int number;
-    printf("Enter a 2-digit number: ");
+    printf("Enter a 3-digit number: ");
     scanf("%d", &number);
 
-    // Extract the two digits
-    int tens_digit = number / 10;
-    int ones_digit = number % 10;
+    // Extract the hundreds digit
+    int hundreds_digit = number / 100;
+
+    // Extract the tens and ones digit
+    int last_2_digits = number  % 100;
+    int tens_digit = last_2_digits / 10;
+    int ones_digit = last_2_digits % 10;
 
     // Reverse the digit
-    int reversed_digit = ones_digit * 10 + tens_digit;
+    int reversed_digit = ones_digit * 100 + tens_digit * 10 + hundreds_digit;
 
     printf("The reversal is: %d\n", reversed_digit);
 
