6d408f838629ee71134550e701f9a79d3e39d2c0
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-12-20 06:24:09 +0800

    feat: Create a program for the Beaufort scale

---
+++ section-5/project-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    float wind_speed;
+    printf("Wind Speed (knots): ");
+    scanf("%f", &wind_speed);
+
+    if (wind_speed < 1) {
+        printf("Calm\n");
+    } else if (wind_speed >= 1 && wind_speed <= 3) {
+        printf("Light air\n");
+    } else if (wind_speed >= 4 && wind_speed <= 27) {
+        printf("Breeze\n");
+    } else if (wind_speed >= 28 && wind_speed <= 47) {
+        printf("Gale\n");
+    } else if (wind_speed >= 48 && wind_speed <= 63) {
+        printf("Storm\n");
+    } else {
+        printf("Hurricane\n");
+    }
+    return 0;
+}
