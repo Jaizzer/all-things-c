2c0ae2fe143eeb4900bd8637773a11cbd7b2ef89
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-12-28 06:53:35 +0800

    feat: Create a mini checkings prgram

---
+++ section-6/checking.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("*** ACME checkbook-balancing program ***\nCommands: 0=clear, 1=credit, 2=debit, "
+           "3=balance, 4=exit\n");
+
+    float balance = 0;
+    float debit = 0;
+    float credit = 0;
+
+    int command;
+    for (;;) {
+        printf("Enter command: ");
+        scanf("%d", &command);
+
+        switch (command) {
+        case 1:
+            printf("Enter amount of credit: ");
+            scanf("%f", &credit);
+            balance += credit;
+
+            break;
+        case 2:
+            printf("Enter amount of debit: ");
+            scanf("%f", &debit);
+            balance -= debit;
+            break;
+
+        case 3:
+            printf("Current Balance: $%0.2f\n", balance);
+            break;
+
+        case 4:
+            goto done;
+
+        default:
+            break;
+        }
+    }
+
+done:
+    printf("You have exited the program.\n");
+    return 0;
+}
