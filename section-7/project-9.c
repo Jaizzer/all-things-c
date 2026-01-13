7eff07217d8116c5787df3707d7591c20ed1fdae
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-01-13 22:37:24 +0800

    feat: Write program that converts a 12-hour time into its 24-hour equivalent

--- section-7/project-9.c
+++ section-7/project-9.c
@@ -1,23 +1,20 @@
-#include <ctype.h>
 #include <stdio.h>
+#include <ctype.h> 
 
 int main(void) {
-    printf("Enter name: ");
-    char current_character;
-    char first_name_initial;
-    scanf("%c", &first_name_initial);
+    // Ask the user for the input
+    int input_hour;
+    int input_minute;
+    char suffix;
+    printf("Enter a 12-hour time: ");
+    scanf("%d:%d %c", &input_hour, &input_minute, &suffix);
 
-    do {
-        scanf("%c", &current_character);
-    } while (current_character != ' ');
-    scanf("%c", &current_character);
+    // Capitalize the suffix
+    suffix = toupper(suffix);
 
-    do {
-        printf("%c", current_character);
-        scanf("%c", &current_character);
-    } while (current_character != '\n');
-
-    printf(", %c.\n", first_name_initial);
-
-    return 0;
+    // Add 12 hours if the suffix contains P
+    if (suffix == 'P') {
+        input_hour += 12;
+    }
+    printf("Equivalent 24-hour time: %d:%02d\n", input_hour, input_minute);
 }
