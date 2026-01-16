7e37449a36908142467280283915bb91797d24e7
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-01-16 21:13:01 +0800

    feat: Write a program that checks if a digit was repeated in the user provided number

---
+++ section-8/repdigit.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void) {
+    char current_digit;
+    int repeat_counter[10] = {0};
+    printf("Enter a number: ");
+
+    for (;;) {
+        scanf("%c", &current_digit);
+        if (current_digit == '\n') {
+            break;
+        } else {
+            repeat_counter[current_digit - '0']++;
+        }
+    }
+
+    bool is_digit_repeated = false;
+    for (int i = 0; i < 10; i++) {
+        if (repeat_counter[i] > 1) {
+            is_digit_repeated = true;
+            break;
+        }
+    }
+
+    if (is_digit_repeated) {
+        printf("Repeated Digit\n");
+    } else {
+        printf("No repeated digit\n");
+    }
+
+    return 0;
+}
