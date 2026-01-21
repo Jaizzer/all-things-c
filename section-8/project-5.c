30751af7f305e9a39c661f455294dc9abff81a44
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-01-21 12:50:40 +0800

    feat: Write progrma that calcualtes the annual interest rate wih monthly interest rate

---
+++ section-8/project-5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define SIZE ((int)(sizeof(interest_rates) / sizeof(interest_rates[0])))
+
+int main(void) {
+    // Ask the user for the interest rate
+    float initial_interest_rate;
+    printf("Enter interest rate: ");
+    scanf("%f", &initial_interest_rate);
+
+    // Ask the user for the number of years
+    int number_of_years;
+    printf("Enter number of years: ");
+    scanf("%d", &number_of_years);
+
+    // Store all 5 interest rates
+    float interest_rates[5];
+    for (int i = 0; i < SIZE; i++) {
+        interest_rates[i] = initial_interest_rate;
+        initial_interest_rate++;
+    }
+
+    printf("YEARS");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%15d%%", (int)interest_rates[i]);
+    }
+    printf("\n");
+
+    // Calculate the investment value
+    float investment_values[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+        investment_values[i] = 100;
+    }
+
+    for (int h = 0; h < number_of_years; h++) {
+        printf("%d", h);
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < 12; j++) {
+                investment_values[i] =
+                    investment_values[i] + investment_values[i] * interest_rates[i] / 100;
+            }
+            printf("\t%15.2f", investment_values[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
