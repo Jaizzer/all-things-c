87206de41a831ae9d331e09fd67fce3864368f59
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-01-26 14:32:24 +0800

    feat: Write a program that performs Caesar cipher on a given message

--- section-8/project-15.c
+++ section-8/project-15.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    char message[100];
+    // Ask the user for the message
+    char message[100] = {0};
     char current_character;
     printf("Enter message to be encrypted: ");
-
     for (int i = 0; i < 100; i++) {
         scanf("%c", &current_character);
         if (current_character == '\n') {
@@ -14,6 +14,30 @@ int main(void) {
         }
     }
 
-    printf("\n");
+    // Ask the user for the shift amount
+    int shift_amount = 0;
+    printf("Enter Shift amount: (1-25): ");
+    scanf("%d", &shift_amount);
+
+    // Encrypt the message.
+    for (int i = 0; i < 100; i++) {
+        if (message[i] == '\0') {
+            printf("\n");
+            break;
+        } else {
+            if (message[i] >= 'A' && message[i] <= 'Z') {
+                int numerical = ((message[i] + shift_amount) / 'Z') * ('A' - 1);
+                current_character = ((message[i] + shift_amount) % ('Z' + 1)) +
+                                    ((message[i] + shift_amount) / ('Z' + 1)) * ('A');
+            } else if (message[i] >= 'a' && message[i] <= 'z') {
+                current_character = ((message[i] + shift_amount) % ('z' + 1)) +
+                                    ((message[i] + shift_amount) / ('z' + 1)) * ('a');
+            } else {
+                current_character = message[i];
+            }
+            printf("%c", current_character);
+        }
+    }
+
     return 0;
 }
