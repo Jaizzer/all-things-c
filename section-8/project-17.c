4fd4478acc7e9a05894aff89f998626fcb34e189
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-02-01 07:10:40 +0800

    feat: Write the input collection logic of project-17

---
+++ section-8/project-17.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    printf("This program creates a magic square of a specified size.\nThe size must be an odd "
+           "number between 1 and 99.\nEnter size of magic square: ");
+    scanf("%d", &n);
+
+    return 0;
+}
