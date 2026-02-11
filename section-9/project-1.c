4e525e03868b1b7a49528f408a9d1c7d28f68f06
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-02-11 12:37:52 +0800

    feat: Write a program that performs selection sort on n integers

--- section-9/project-1.c
+++ section-9/project-1.c
@@ -1,16 +1,49 @@
 #include <stdio.h>
 
-int selection_sort(int numbers[]);
+void selection_sort(int numbers[], int starting_index, int array_size);
 
 int main(void) {
+    // Collect the integers to be sorted
     int n = 10;
     int integers[n];
     printf("Enter a series of %d integers to be sorted: ", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &integers[i]);
     }
+    
+    // Sort the array
+    selection_sort(integers, 0, n);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", integers[i]);
+    }
+    printf("\n");
 
     return 0;
 }
 
-int selection_sort(int numbers[]) { return 0; }
+void selection_sort(int numbers[], int starting_index, int array_size) {
+    // Return if the array contains 1 element
+    if (starting_index == array_size - 1) {
+        return;
+    }
+
+    // Find the smallest element
+    int smallest = starting_index;
+    for (int i = starting_index; i < array_size; i++) {
+        if (numbers[smallest] > numbers[i]) {
+            smallest = i;
+        }
+    }
+
+    // Swap the smallest element to the current array's first element
+    int temp = numbers[starting_index];
+    numbers[starting_index] = numbers[smallest];
+    numbers[smallest] = temp;
+
+    // Sort the smaller array
+    selection_sort(numbers, starting_index + 1, array_size);
+
+    return;
+}
