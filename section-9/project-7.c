46fe768e6bd0749d1173141b1ef16246c8b780cc
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-02-17 08:10:22 +0800

    feat: Write a program that simulates the game of craps

--- section-9/project-7.c
+++ section-9/project-7.c
@@ -4,10 +4,63 @@
 #include <time.h>
 
 int roll_dice(void);
-int main(void) { return 0; }
+bool play_game(void);
+
+int main(void) {
+    bool result;
+    int wins = 0;
+    int losses = 0;
+    char response;
+
+    for (;;) {
+        result = play_game();
+        if (result) {
+            printf("You win.\n");
+            wins++;
+        } else {
+            printf("You lose.\n");
+            losses++;
+        }
+
+        printf("\nPlay again: ");
+        scanf(" %c", &response);
+        if (response != 'y' && response != 'Y') {
+            printf("Wins: %d\tLosses: %d\n", wins, losses);
+            break;
+        }
+    }
+
+    return 0;
+}
 
 int roll_dice(void) {
     // Seed the random function
     srand(time(NULL));
-    return (rand() % 6) + 1;
+    return ((rand() % 6) + 1) + ((rand() % 6) + 1);
+}
+
+bool play_game(void) {
+    int sum = roll_dice();
+    printf("You rolled: %d\n", sum);
+
+    int point;
+    if (sum == 7 || sum == 8) {
+        return true;
+    } else if (sum == 2 || sum == 3 || sum == 12) {
+        return false;
+    } else {
+        point = sum;
+        printf("You point is %d\n", point);
+        do {
+            sum = roll_dice();
+            printf("You rolled: %d\n", sum);
+
+            if (sum == 7) {
+                return false;
+            }
+
+        } while (sum != point);
+    }
+
+    return true;
 }
