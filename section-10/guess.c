bfb9d5b543f282f0ec4e18adbd1a2e4128d1c70a
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-02-19 22:03:17 +0800

    feat: Write the initial number guessing program

--- section-10/guess.c
+++ section-10/guess.c
@@ -6,7 +6,34 @@
 int generate_random_number(void);
 
 int main(void) {
-    int number = generate_random_number();
+    int number;
+    int number_of_guesses;
+    char response;
+    int guess;
+    int guess_count = 0;
+    while (true) {
+        number = generate_random_number();
+        while (true) {
+            printf("Enter guess: ");
+            scanf("%d", &guess);
+            guess_count++;
+            if (guess > number) {
+                printf("Too high; try again.\n");
+            } else if (guess < number) {
+                printf("Too low; try again.\n");
+            } else {
+                printf("You won in %d guesses!\n", guess_count);
+                break;
+            }
+        }
+
+        // Ask the user if they want to play again.
+        printf("\nPlay again? (Y/N) ");
+        scanf(" %c", &response);
+        if (response != 'y' && response != 'Y') {
+            break;
+        }
+    }
     return 0;
 }
 int generate_random_number(void) {
