ace201a1089c140029c4b16fce711cde2cc0a346
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-02-21 23:18:24 +0800

    feat: Create a function that reads all the cards in the hand

--- section-10/poker.c
+++ section-10/poker.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#define NUMBER_OF_CARDS 5
+
+void read_hand(char hand[][2]);
 
 int main(void) {
-    char card[5][2] = {0};
-    for (int i = 0; i < 5; i++) {
+    char hand[NUMBER_OF_CARDS][2] = {0};
+    read_hand(hand);
+    for (int i = 0; i < NUMBER_OF_CARDS; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%c", hand[i][j]);
+        }
+        printf(" ");
+    }
+    printf("\n");
+    return 0;
+}
+
+void read_hand(char hand[][2]) {
+    for (int i = 0; i < NUMBER_OF_CARDS; i++) {
         printf("Enter a card: ");
         for (int j = 0; j < 2; j++) {
-            scanf(" %c", &card[i][j]);
+            scanf(" %c", &hand[i][j]);
         }
     }
-
-    return 0;
 }
